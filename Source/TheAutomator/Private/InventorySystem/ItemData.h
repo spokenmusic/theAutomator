06811bb9b82792c4e5e7bf062fb4a7caa1f0a0ca
Leo Arguello <larguello@blindsquirrelgames.com>
2025-07-10 10:47:09 -0600

    Made the project c++

---
+++ Source/TheAutomator/Private/InventorySystem/ItemData.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+USTRUCT(Blueprintable)
+struct FItemData
+{
+	GENERATED_USTRUCT_BODY();
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	FName ItemID;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	FText DisplayName;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	UTexture2D* Texture;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	int32 MaxStackSize;
+};
---
+++ Source/TheAutomator/TheAutomator.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheAutomator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheAutomator, "TheAutomator" );
